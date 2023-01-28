0f2b8f368f4547cda277081257527e91676fccfd
mpaunkov <78094343+mpaunkov@users.noreply.github.com>
2023-01-28 23:15:50 +0200

    Add project files.

---
+++ Collection.Unit.Tests/CollectionTests.cs
@@ -0,0 +1,12 @@
+namespace Collection.Unit.Tests
+{
+    public class CollectionTests
+    {
+      
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Collection/Program.cs
@@ -0,0 +1,28 @@
+﻿using Collections;
+
+namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new Collection<int>();
+            Console.WriteLine("Current collection: " + collection.ToString());
+            Console.WriteLine("Collection count: " + collection.Count);
+            Console.WriteLine("Collection capacity: " + collection.Capacity);
+            collection.Add(5);
+            Console.WriteLine("Current collection: " + collection);
+            collection.AddRange(6, 7);
+            Console.WriteLine("Current collection: " + collection);
+            Console.WriteLine("Print the first element: " + collection[0]);
+            collection[0] = 55;
+            Console.WriteLine("Print the first element: " + collection[0]);
+            collection.InsertAt(2, 666);
+            Console.WriteLine("Current collection: " + collection);
+            collection.Exchange(1, 3);
+            Console.WriteLine("Current collection: " + collection);
+            collection.Clear();
+            Console.WriteLine("Current collection: " + collection);
+        }
+    }
+}
